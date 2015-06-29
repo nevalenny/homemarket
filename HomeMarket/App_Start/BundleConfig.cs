5a57c0aa3a16889bc10b4e543597de2810d20abb
Denis Miroshnikov <denis.miroshnikov@bizmosis.com>
2015-06-29 20:34:22 +0300

    Partially set up dropzone for images

--- HomeMarket/App_Start/BundleConfig.cs
+++ HomeMarket/App_Start/BundleConfig.cs
@@ -41,6 +41,13 @@ namespace HomeMarket
                 .Include("~/Content/site.css")
                 .Include("~/Content/bootstrap*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/dropzonescripts").Include(
+                     "~/Scripts/dropzone/dropzone.js"));
+
+            bundles.Add(new StyleBundle("~/Content/dropzonescss").Include(
+                     "~/Scripts/dropzone/css/basic.css",
+                     "~/Scripts/dropzone/css/dropzone.css"));
+
         }
     }
 }
